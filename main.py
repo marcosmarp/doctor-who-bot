b55d196a60fdfe8eb07e1353e28f1bf71f452c79
Marcos <marcosmaritnezplermo@gmail.com>
2021-10-09 05:48:27 -0300

    Fixed typo on subreddit name

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from functions import RunBot, MinutesBetweenTimes, InitPraw
 
 last_comment_time = datetime(1990, 10, 9, 4, 19, 15) # Making sure that first loop runs and the time check begins with the second loop
 reddit_handler = InitPraw()
-doctor_who_subreddit_handler = reddit_handler.subreddit("doctor-who-bot")
+doctor_who_subreddit_handler = reddit_handler.subreddit("doctorwho")
 
 while(True):
   if MinutesBetweenTimes(last_comment_time, datetime.now()) >= 10:
